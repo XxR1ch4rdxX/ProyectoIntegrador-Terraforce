0f78b197aeafe7c7811043f4c571671e6c7ba0f3
XxR1ch4rdxX <lollespartam@gmail.com>
2024-06-27 12:04:07 -0600

    creamos mas htmls y la iniciativa conexion con sql

--- CRUD/flaskProject/app.py
+++ CRUD/flaskProject/app.py
@@ -5,13 +5,24 @@
 #ESTE ES EL CORAZON DE LA PAGINA WEB (por asi decirlo)
 
 from flask import Flask,render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
+def index():  # put application's code here
+    titulo = 'TerraForce'
+    return render_template(template_name_or_list='index.html',titulo=titulo)
+
+@app.route('/signup')
+def signup():  # put application's code here
+    titulo = 'TerraForce'
+    return render_template(template_name_or_list='signup.html',titulo=titulo)
+
+@app.route('/about')
+def about():  # put application's code here
+    titulo = 'TerraForce'
+    return render_template(template_name_or_list='about.html',titulo=titulo)
 
 
 if __name__ == '__main__':
