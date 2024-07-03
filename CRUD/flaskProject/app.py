c615a918dc87f626b6a7e958e9ebbde5f4dd7bbe
XxR1ch4rdxX <lollespartam@gmail.com>
2024-07-03 09:52:40 -0600

    obtenemos los nombres de las tablas
    ya se pueden ver los nombres de las tablas en el crud, falta seleccionarlas y traer los datos de esas tablas para poder hacer altas bajas y cambios

--- CRUD/flaskProject/app.py
+++ CRUD/flaskProject/app.py
@@ -14,10 +14,11 @@ init()
 
 
 app = Flask(__name__)
-
+server='PCerda'
+database='TerraForce'
 try:
     print(Fore.CYAN+'Estableciendo la conexion con sql server ...'+Style.RESET_ALL)
-    connection = pyodbc.connect('DRIVER={SQL Server}; SERVER=PCerda; DATABASE=TerraForce; Trusted_Connection=yes;')
+    connection = pyodbc.connect('DRIVER={SQL Server}; SERVER='+server+'; DATABASE='+database+'; Trusted_Connection=yes;')
     print(Fore.GREEN+'Tamo en linea '+Style.RESET_ALL)
     cursor = connection.cursor()
     cursor.execute("SELECT @@version;")
@@ -38,12 +39,33 @@ except pyodbc.Error as e:
 @app.route('/')
 def index():  # put application's code here
     titulo = 'TerraForce'
-    return render_template(template_name_or_list='index.html',titulo=titulo)
+
+    return render_template(template_name_or_list='index.html',titulo=titulo )
 
 @app.route('/crud')
 def signup():  # put application's code here
     titulo = 'TerraForce'
-    return render_template(template_name_or_list='signup.html',titulo=titulo)
+    try:
+        cursor.execute("""
+                SELECT TABLE_NAME
+                FROM INFORMATION_SCHEMA.TABLES
+                WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG = ?
+                ORDER BY TABLE_NAME ASC;
+            """, (database,))
+
+        tablas = [row.TABLE_NAME for row in cursor.fetchall()]
+        return render_template('crud.html', titulo=titulo, tablas=tablas)
+
+    except pyodbc.Error as e:
+        error_message = f"Error al recuperar las tablas: {str(e)}"
+        return render_template('error.html', error_message=error_message)
+
+
+
+@app.route('/tablas/<nombretabla>')
+def ver_tablas(nombretabla):
+    return f'Seleccionaste la tabla {nombretabla}'
+
 
 @app.route('/about')
 def about():  # put application's code here
